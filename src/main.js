27a8043a913d994624bf93e845e59ee194a4f0ef
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-08-15 23:54:11 -0300

    segunda página Personagens com card aparecendo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,48 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+const sectionConteudos = document.querySelector('.conteudos');
+const url = 'got.json';
 
-console.log(example, data);
+function pegarDados() {
+    fetch(url)
+        .then(response => response.json())
+        .then(dados => {
+            if (dados.erro) {
+                console.log("Erro ao acessar o JSON");
+                return;
+            }
+            desenharCartas(dados.got);
+        })
+        .catch(error => {
+            console.error("Erro ao carregar os dados:", error);
+        });
+}
 
+function desenharCartas(personagens) {
+    sectionConteudos.innerHTML = ''; // Limpa o conteúdo anterior
+    personagens.forEach(personagem => {
+        const carta = document.createElement("article");
+        carta.setAttribute('class', 'card');
+        sectionConteudos.appendChild(carta);
+
+        const imagem = document.createElement("img");
+        imagem.setAttribute('class', 'imageUrl');
+        imagem.setAttribute('src', personagem.imageUrl); // Correção aqui
+        carta.appendChild(imagem);
+
+        const nomePersonagem = document.createElement("h2");
+        nomePersonagem.setAttribute('class', 'fullName');
+        nomePersonagem.textContent = personagem.fullName;
+        carta.appendChild(nomePersonagem);
+
+        const familia = document.createElement("h3");
+        familia.setAttribute('class', 'family');
+        familia.textContent = personagem.family;
+        carta.appendChild(familia);
+
+        const titulo = document.createElement("h3");
+        titulo.setAttribute('class', 'title');
+        titulo.textContent = personagem.title;
+        carta.appendChild(titulo);
+    });
+}
+
+pegarDados();
