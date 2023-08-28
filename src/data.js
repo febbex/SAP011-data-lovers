99950e84418ff61c7f990b99e58af8ce4def6984
Dahbarbara <dahbarbara_@hotmail.com>
2023-08-28 10:37:33 -0300

    função no data

--- src/data.js
+++ src/data.js
@@ -1,11 +1,32 @@
-// estas funciones son de ejemplo
+// O módulo data.js exporta duas funções para serem usadas em outras partes do código
 
-//export const example = () => {
-//    return 'example';
-// };
- 
-// export const anotherExample = () => {
-//  return 'OMG';
-//};
-//filtro familia
+// Função para gerar opções de filtro de família no elemento select
+export function generateFamilySelectOptions(data, familySelect) {
+  // Cria um conjunto de todas as famílias presentes nos dados
+  const families = new Set(data.map(character => character.family));
+
+  // Itera sobre as famílias e cria uma opção para cada uma no elemento select
+  families.forEach(family => {
+      const option = document.createElement('option');
+      option.value = family; // Define o valor da opção como o nome da família
+      option.textContent = family; // Define o texto da opção como o nome da família
+      familySelect.appendChild(option); // Adiciona a opção ao elemento select
+  });
+}
+
+// Função para filtrar e ordenar os personagens com base nos critérios
+export function filterAndSortCharacters(inputValue, selectedFamily, sortOrder, characters) {
+  // Filtra os personagens com base no nome e família
+  const filteredCharacters = characters.filter(character => {
+      const nameMatches = character.fullName.toLowerCase().includes(inputValue.toLowerCase());
+      const familyMatches = selectedFamily ? character.family === selectedFamily : true;
+      return nameMatches && familyMatches;
+  });
+
+  // Realiza uma cópia dos personagens filtrados e os ordena com base na ordem fornecida
+  return filteredCharacters.slice().sort((a, b) => {
+      const comparison = a.fullName.localeCompare(b.fullName);
+      return sortOrder === 'asc' ? comparison : -comparison;
+  });
+}
 
--- src/main.js
+++ src/main.js
@@ -1,87 +1,62 @@
-// Importando os dados de personagens de um arquivo got.js
+// Importando o objeto de dados e as funções necessárias do módulo data.js
 import data from './data/got/got.js';
+import { generateFamilySelectOptions, filterAndSortCharacters } from './data.js';
 
-// Selecionando o elemento que conterá os cards dos personagens
+// Selecionando elementos do DOM
 const sectionCards = document.querySelector('.cardPersonagens');
-
-// Selecionando os elementos usados para filtrar e ordenar os personagens
 const filtrarPorNome = document.querySelector('#filter');
 const familySelect = document.querySelector('#filtrarPorFamilia');
 const sortSelect = document.querySelector('#filtrarOrdem');
 
-// Função que é chamada inicialmente para iniciar a renderização dos cards
-function initialize() {
-  renderCards(); // Renderiza os cards na página
-  generateFamilySelectOptions(); // Gera opções para o filtro de família
+// Função para renderizar os cards dos personagens
+function renderCharacterCards(characters) {
+    sectionCards.innerHTML = ''; // Limpa os cards existentes na seção
+
+    // Itera pelos personagens e cria um card para cada um
+    characters.forEach(character => {
+        const card = createCharacterCard(character);
+        sectionCards.appendChild(card); // Adiciona o card à seção
+    });
 }
 
-// Gera as opções do filtro de família com base nos dados
-function generateFamilySelectOptions() {
-  // Cria um conjunto (conjunto de valores únicos) de todas as famílias dos personagens
-  const families = new Set(data.got.map(character => character.family));
-  
-  // Itera sobre as famílias e cria uma opção para cada uma no elemento select
-  families.forEach(family => {
-      const option = document.createElement('option');
-      option.value = family; // Valor da opção é a família
-      option.textContent = family; // Texto da opção é a família
-      familySelect.appendChild(option); // Adiciona a opção ao elemento select
-  });
-}
-
-// Função que filtra e renderiza os cards de acordo com inputs e seleções
-function filterCardsByInputAndFamily() {
-  const inputValue = filtrarPorNome.value; // Valor do input de filtragem por nome
-  const selectedFamily = familySelect.value; // Valor do filtro de família selecionado
-  const sortOrder = sortSelect.value; // Valor do filtro de ordenação selecionado
-
-  // Função de filtro que verifica se nome e família correspondem aos critérios
-  const filterFn = character => {
-      const nameMatches = character.fullName.toLowerCase().includes(inputValue.toLowerCase());
-      const familyMatches = selectedFamily ? character.family === selectedFamily : true;
-      return nameMatches && familyMatches;
-  };
-
-  // Copia os personagens para ordenação e realiza a ordenação com base na ordem selecionada
-  const sortedCharacters = data.got.slice().sort((a, b) => {
-      const comparison = a.fullName.localeCompare(b.fullName);
-      return sortOrder === 'asc' ? comparison : -comparison;
-  });
-
-  // Renderiza os cards filtrados e ordenados
-  renderCards(filterFn, sortedCharacters);
-}
-
-// Renderiza os cards dos personagens
-function renderCards(filterFn = null, characters = data.got) {
-  sectionCards.innerHTML = ''; // Limpa os cards existentes na seção
-
-  // Filtra os personagens de acordo com a função de filtro (se fornecida)
-  const filteredCharacters = filterFn ? characters.filter(filterFn) : characters;
-
-  // Itera pelos personagens filtrados e cria um card para cada um
-  filteredCharacters.forEach(character => {
-      const card = createCharacterCard(character);
-      sectionCards.appendChild(card); // Adiciona o card à seção
-  });
-}
-
-// Cria um elemento de card para um personagem
+// Função para criar um card de personagem
 function createCharacterCard(character) {
-  const card = document.createElement('div');
-  card.classList.add('card'); // Adiciona a classe "card" ao elemento div
-  card.innerHTML =
-      `<img src="${character.imageUrl}" alt="${character.fullName}"/>
-      <h2 class='nome'>${character.fullName}</h2>
-      <h3 class='familia'>${character.family}</h3>
-      <h3 class='titulo'>${character.title}</h3>`;
-  return card; // Retorna o elemento card criado
+    const card = document.createElement('div');
+    card.classList.add('card'); // Adiciona a classe "card" ao elemento div
+    card.innerHTML =
+        `<img src="${character.imageUrl}" alt="${character.fullName}"/>
+        <h2 class='nome'>${character.fullName}</h2>
+        <h3 class='familia'>${character.family}</h3>
+        <h3 class='titulo'>${character.title}</h3>`;
+    return card; // Retorna o elemento card criado
 }
 
-// Adiciona ouvintes de eventos para os inputs e selects
-filtrarPorNome.addEventListener('input', filterCardsByInputAndFamily);
-familySelect.addEventListener('change', filterCardsByInputAndFamily);
-sortSelect.addEventListener('change', filterCardsByInputAndFamily);
+// Função de inicialização
+function initialize() {
+    // Gera as opções de filtro de família no elemento select
+    generateFamilySelectOptions(data.got, familySelect);
 
-// Inicializa a página chamando a função initialize
+    // Função de listener que é chamada quando ocorrem alterações nos filtros
+    const filterListener = () => {
+        const inputValue = filtrarPorNome.value; // Valor do input de filtragem por nome
+        const selectedFamily = familySelect.value; // Valor do filtro de família selecionado
+        const sortOrder = sortSelect.value; // Valor do filtro de ordenação selecionado
+
+        // Filtra e ordena os personagens com base nos critérios escolhidos
+        const filteredAndSorted = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
+
+        // Renderiza os cards dos personagens filtrados e ordenados
+        renderCharacterCards(filteredAndSorted);
+    };
+
+    // Adiciona ouvintes de eventos aos elementos de input e select
+    filtrarPorNome.addEventListener('input', filterListener); // Evento de input no campo de filtragem
+    familySelect.addEventListener('change', filterListener); // Evento de seleção da família
+    sortSelect.addEventListener('change', filterListener); // Evento de seleção da ordenação
+
+    // Renderiza inicialmente os cards com todos os personagens
+    renderCharacterCards(data.got);
+}
+
+// Inicializa a página
 initialize();
