4746f1ac706f9f1375b3906e89681198b1270bf3
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-08-30 13:11:03 -0300

    atualização com responsivo em andamento e teste implementado

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,21 +1,40 @@
-import { generateFamilySelectOptions } from './data.js';
+import data from '../src/data/got/got.js';
+import { filterAndSortCharacters } from '../src/data.js';
 
-describe('generateFamilySelectOptions', () => {
-  it('retornar familias', () => {
-    const data = [
-      { family: 'THouse Targaryen' },
-      { family: 'House Stark' },
-      { family: 'House Lannister'},
+describe('filterAndSortCharacters', () => {
+  it('Filtrar por familia e ordem', () => {
+    const inputValue = 'Jon';
+    const selectedFamily = 'House Stark';
+    const sortOrder = 'asc';
 
-    ];
+    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
 
-    const familySelect = document.createElement('select');
-    generateFamilySelectOptions(data, familySelect);
+    expect(result.length).toBe(1);
+    expect(result[0].fullName).toBe('Jon Snow');
+  });
 
-    const options = familySelect.querySelectorAll('option');
+  it('retornar personagens com o filtro aplicado', () => {
+    const inputValue = '';
+    const selectedFamily = '';
+    const sortOrder = 'asc';
 
-    const optionValues = Array.from(options).map(option => option.value);
+    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
 
-    expect(optionValues).toEqual(['', 'House Targaryen', 'House Stark', 'House Lannister']);
+    expect(result.length).toBe(data.got.length);
+    expect(result[0].fullName).toBe('Daenerys Targaryen');
+    expect(result[1].fullName).toBe('Samwell Tarly');
+  });
+
+  it('retorna personagens em ordem decrescente', () => {
+    const inputValue = '';
+    const selectedFamily = '';
+    const sortOrder = 'desc';
+
+    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
+
+    expect(result.length).toBe(data.got.length);
+    expect(result[0].fullName).toBe('Daenerys Targaryen');
+    expect(result[1].fullName).toBe('Viserys Targaryn');
+  
   });
 });
