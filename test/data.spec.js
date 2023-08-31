bc6d71d98d725638e1336734270b5af802898b53
Thamires Febbo <thamiresfebbo@hotmail.com>
2023-08-31 17:23:56 -0300

    testes ok ajustados, css com resposividade ok

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,40 +1,70 @@
-import data from '../src/data/got/got.js';
 import { filterAndSortCharacters } from '../src/data.js';
 
+const characters = 
+  [
+    {
+      "firstName": "Arya",
+      "fullName": "Arya Stark",
+      "family": "House Stark",
+    },
+    {
+      "firstName": "Jon",
+      "fullName": "Jon Snow",
+      "family": "House Stark",
+    },
+    {
+      "firstName": "Brandon",
+      "fullName": "Brandon Stark",
+      "family": "House Stark",
+    },
+    {
+      "firstName": "Ygritte",
+      "fullName": "Ygritte",
+      "family": "Free Folk",
+    },
+    {
+      "firstName": "Yara",
+      "fullName": "Yara Greyjoy",
+      "family": "Greyjoy",
+    }
+  ]
+
 describe('filterAndSortCharacters', () => {
-  it('Filtrar por familia e ordem', () => {
+  it('Filtrar por familia', () => {
     const inputValue = 'Jon';
     const selectedFamily = 'House Stark';
     const sortOrder = 'asc';
 
-    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
+    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, characters);
 
     expect(result.length).toBe(1);
     expect(result[0].fullName).toBe('Jon Snow');
   });
 
-  it('retornar personagens com o filtro aplicado', () => {
+  it('retornar personagens ordem crescente', () => {
     const inputValue = '';
     const selectedFamily = '';
     const sortOrder = 'asc';
 
-    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
+    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, characters);
 
-    expect(result.length).toBe(data.got.length);
-    expect(result[0].fullName).toBe('Daenerys Targaryen');
-    expect(result[1].fullName).toBe('Samwell Tarly');
+    expect(result.length).toBe(characters.length);
+    expect(result[0].fullName).toBe('Arya Stark');
+    expect(result[1].fullName).toBe('Brandon Stark');
   });
 
-  it('retorna personagens em ordem decrescente', () => {
+  it('retornar personagens ordem decrescente', () => {
     const inputValue = '';
     const selectedFamily = '';
     const sortOrder = 'desc';
 
-    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, data.got);
+    const result = filterAndSortCharacters(inputValue, selectedFamily, sortOrder, characters);
 
-    expect(result.length).toBe(data.got.length);
-    expect(result[0].fullName).toBe('Daenerys Targaryen');
-    expect(result[1].fullName).toBe('Viserys Targaryn');
-  
+    expect(result.length).toBe(characters.length);
+    expect(result[0].fullName).toBe('Ygritte');
+    expect(result[1].fullName).toBe('Yara Greyjoy');
+
   });
+
 });
+
